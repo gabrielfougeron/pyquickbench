3e29253052416396f788e81e0195b4d2335bce62
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2025-09-22 23:22:02 +0200

    more CLI

--- pyquickbench/compare_app/CLI.py
+++ pyquickbench/compare_app/CLI.py
@@ -9,8 +9,21 @@ from pyquickbench.manual_rank_assign import ManualRankAssign
 from rich.text import Text
 from rich.style import Style
 
+# from textual import getters
 from textual.app import App
 from textual.widgets import Footer, Header, Tree
+from textual.widgets import Input, Label, DirectoryTree, Select
+from textual.validation import Number, Function
+from textual.containers import Horizontal, Vertical
+
+cycle_intents = {
+    "compare" : "group",
+    "group" : "compare",
+}
+intents_label_color = {
+    "compare" : "green",
+    "group" : "orange1",
+}
 
 class ImageCompareCLI(App):
     
@@ -18,8 +31,9 @@ class ImageCompareCLI(App):
 
     BINDINGS = [
         # ("d", "toggle_dark", "Toggle dark mode"),
-        ("ctrl+Return", "toggle_dark", "Toggle dark mode"),
-        ("a", "toggle_select", "Toggle group / compare, restrict / unrestrict"),
+        # ("ctrl+Return", "toggle_dark", "Toggle dark mode"),
+        ("left", "toggle_select", "Toggle group / compare, restrict / unrestrict"),
+        ("right", "toggle_select", "Toggle group / compare, restrict / unrestrict"),
     ]
     
     def __init__(self, Workspace_dir):
@@ -28,31 +42,32 @@ class ImageCompareCLI(App):
         self.messages = []
         
         self.Workspace_dir = Workspace_dir
-        
-        self.bench_filename = os.path.join(self.Workspace_dir, "bench.npz")
-        
-        if not os.path.isfile(self.bench_filename):
-            raise ValueError(f"Benchmark file {self.bench_filename} not found.")
 
-        self.benchfile_shape, self.all_vals = run_benchmark(
-            filename = self.bench_filename  ,
+        self.cur_highlighted_node = None
+        self.cur_focus_id = None
+        
+    @classmethod
+    def load_benchfile(self, bench_filename):
+        return run_benchmark(
+            filename = bench_filename       ,
             return_array_descriptor = True  ,
             StopOnExcept = True             ,
         )
         
-        self.restrict_idx, self.restrict_shape = ManualRankAssign.default_restrict_values(self.benchfile_shape)
-        self.compare_intent  = ManualRankAssign.default_compare_intent(self.benchfile_shape)
+    def populate_bench_tree(self, tree = None):
         
-        self.cur_highlighted_node = None
-        
-    def build_bench_tree(self):
-        
-        tree = Tree("Benchmark")
-        tree.root.expand()
-        
+        if tree is None:
+            tree = self.query_one("#bench_tree")
+            
+        tree.clear()
+
         for i, (key, vals) in enumerate(self.benchfile_shape.items()):
         
-            keytree = tree.root.add(label = f"{key} : {self.compare_intent[key]}", data = key)
+            intent = self.compare_intent[key]
+            label = Text(f"{key}")
+            label.append_text(Text(f"    {intent}", style = Style(color = intents_label_color[intent])))
+        
+            keytree = tree.root.add(label = label, data = key)
             
             if hasattr(vals, '__getitem__'): 
                 vals_it = vals
@@ -62,79 +77,120 @@ class ImageCompareCLI(App):
                 vals_it = range(vals)
                 
             for j, val in enumerate(vals_it):
+
+                label = Text(f"{val}")
                 
                 if j in self.restrict_idx[i]:
-                    label = f"{val} : selected"
+                    label.append_text(Text("    selected", style = Style(color = "green")))
                 else:
-                    label = f"{val} : unselected"
-                
+                    label.append_text(Text("    unselected", style = Style(color = "red")))
+
                 keytree.add_leaf(label = label, data = (key, i, j))
 
-        return tree
-        
     def compose(self):
         """Create child widgets for the app."""
-        yield Header()
-        yield self.build_bench_tree()
-        yield Footer()
 
-    def on_tree_node_highlighted(self, event):
+        yield Header()
+
+        filelist = [f for f in os.listdir(self.Workspace_dir) if os.path.isfile(f)]
+        if len(filelist) == 0:
+            filelist = ["Directory is empty"]
+        if "bench.npz" in filelist:
+            value = "bench.npz"
+        else:
+            value = Select.BLANK
+
+        with Horizontal(classes="maxheight"):
+            yield Label("Benchmark file")
+            yield Select.from_values(filelist, value = value)
         
+        with Horizontal(classes="maxheight"):
+            yield Label("Number of options in a comparison")
+            yield Input(placeholder="Enter a number...", value = "2", validators=[Number(minimum=2)])
+            yield Label()
+        
+        tree = Tree("Benchmark", id="bench_tree")
+        tree.show_root = False
+        tree.root.expand()
+        
+        # with Horizontal():
+        yield Label("Benchmark options")
+        yield Label()
+        yield tree
+
+        yield Footer()
+        
+    def on_select_changed(self, event):
+
+        self.bench_filename = os.path.join(self.Workspace_dir, event.value)
+        self.benchfile_shape, self.all_vals = self.load_benchfile(self.bench_filename)
+        self.restrict_idx, self.restrict_shape = ManualRankAssign.default_restrict_values(self.benchfile_shape)
+        self.compare_intent  = ManualRankAssign.default_compare_intent(self.benchfile_shape)
+        
+        try:
+            self.populate_bench_tree()
+        except Exception as exc:
+            self.notify(f'{exc}', timeout=60)
+        
+    def on_tree_node_highlighted(self, event):
         self.cur_highlighted_node = event.node
-    
+        
     def action_toggle_select(self):
         
         if self.cur_highlighted_node is not None:
             
-            data = self.cur_highlighted_node.data
-            
-            if isinstance(data, str):
+            if self.cur_highlighted_node.tree.id == "bench_tree":
                 
-                cur_intent = self.compare_intent[data]
-                if cur_intent == "group":
-                    self.compare_intent[data] = "compare"
-                elif cur_intent == "compare":
-                    self.compare_intent[data] = "group"
-                else:
-                    raise ValueError(f"Unknown comare intent {cur_intent}")
+                data = self.cur_highlighted_node.data
                 
-                label = f"{data} : {self.compare_intent[data]}"
-                                    
-            elif isinstance(data, tuple):
-                
-                key, i, j = data
-                vals = self.benchfile_shape[key]
-                
-                if hasattr(vals, '__getitem__'): 
-                    vals_it = vals
-                elif hasattr(vals, '__len__'):
-                    vals_it = range(len(vals))
-                else:
-                    vals_it = range(vals)
+                if data is not None:
                     
-                val = vals_it[j]
-                
-                idx = bisect.bisect_left(self.restrict_idx[i], j)
+                    if isinstance(data, str):
+                        
+                        cur_intent = self.compare_intent[data]
+                        new_intent = cycle_intents[cur_intent]
+                        self.compare_intent[data] = new_intent
+                        
+                        label = Text(f"{data}")
+                        label.append_text(Text(f"    {new_intent}", style = Style(color = intents_label_color[new_intent])))
+                                            
+                    elif isinstance(data, tuple):
+                        
+                        key, i, j = data
+                        vals = self.benchfile_shape[key]
+                        
+                        if hasattr(vals, '__getitem__'): 
+                            vals_it = vals
+                        elif hasattr(vals, '__len__'):
+                            vals_it = range(len(vals))
+                        else:
+                            vals_it = range(vals)
+                            
+                        val = vals_it[j]
+                        
+                        idx = bisect.bisect_left(self.restrict_idx[i], j)
 
-                isIn = (idx != self.restrict_shape[i])
-                if isIn:
-                    isIn = (self.restrict_idx[i][idx] == j)
+                        isIn = (idx != self.restrict_shape[i])
+                        if isIn:
+                            isIn = (self.restrict_idx[i][idx] == j)
 
-                if isIn:
-                    self.restrict_shape[i] -= 1
-                    self.restrict_idx[i].pop(idx)
-                    label = f"{val} : unselected"
+                        label = Text(f"{val}")
 
-                else:
-                    self.restrict_shape[i] += 1
-                    self.restrict_idx[i].insert(idx, j)
-                    label = f"{val} : selected"
+                        if isIn:
+                            self.restrict_shape[i] -= 1
+                            self.restrict_idx[i].pop(idx)
+                            label.append_text(Text("    unselected", style = Style(color = "red")))
 
-            else:
-                
-                raise ValueError(f'Unknown data type : {type(data)}')
-            
-            self.cur_highlighted_node.set_label(label)
+                        else:
+                            self.restrict_shape[i] += 1
+                            self.restrict_idx[i].insert(idx, j)
+                            label.append_text(Text("    selected", style = Style(color = "green")))
+
+                    else:
+                        
+                        raise ValueError(f'Unknown data type : {type(data)}')
+                    
+                    self.cur_highlighted_node.set_label(label)
 
     def action_toggle_dark(self):
         """An action to toggle dark mode."""
